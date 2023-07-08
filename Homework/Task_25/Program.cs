efcee33a71c0c22353c5c63e4edc5b5b70782413
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-08 15:52:25 +0300

    CommitTask_25

---
+++ Homework/Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Вариант решения №1:
+// Console.Write("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int product = 1;
+// for (int i=1; i<= B; i++)
+// {
+//      product = product * A;
+// }
+// Console.WriteLine($"Число {A} в степини {B} равно: {product}");
+
+// Вариант решения №2:
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {A} в степини {B} равно: {Math.Pow(A, B)}");
--- Program.cs
+++ Program.cs
@@ -45,10 +45,10 @@
 // Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-int size = 8;
-int [] array = new int [size];
-for (int i = 0; i<array.Length; i++)
-{
-    array [i] = new Random().Next(2);
-}
-Console.WriteLine ($"Массив: [ {string.Join("; ", array)} ]");
+// int size = 8;
+// int [] array = new int [size];
+// for (int i = 0; i<array.Length; i++)
+// {
+//     array [i] = new Random().Next(2);
+// }
+// Console.WriteLine ($"Массив: [ {string.Join("; ", array)} ]");
