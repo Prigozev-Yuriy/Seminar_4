01868b3c1ea075a296503425c3ed92fcadda0e76
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-08 16:25:33 +0300

    CommitTask_29

--- Homework/Task_27/Program.cs
+++ Homework/Task_27/Program.cs
@@ -3,13 +3,13 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int CopyNumber = number;
-int sum = 0;
-while (CopyNumber > 0)
-{
-    sum = sum + CopyNumber % 10;
-    CopyNumber = CopyNumber / 10;
-}
-Console.Write($"Сумма цифр в числе {number} равна: {sum}");
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int CopyNumber = number;
+// int sum = 0;
+// while (CopyNumber > 0)
+// {
+//     sum = sum + CopyNumber % 10;
+//     CopyNumber = CopyNumber / 10;
+// }
+// Console.Write($"Сумма цифр в числе {number} равна: {sum}");
---
+++ Homework/Task_29/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Или массив из 8 элементов вводится с консоли (каждый элемент вводит человек)
+
+// Если массив наполняется рандомом:
+int size = 8;
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+      array[i] = new Random().Next(10, 1000);
+}
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+
+
+
+// Если массив вводистя вручную:
+// int size = 8;
+// int[] array = new int[size];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write("Введие число: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
